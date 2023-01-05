3266379d6d36afa8928feabe6b9f4cbb4386d287
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-01-05 15:59:09 +0700

    tset

--- connect.php
+++ connect.php
@@ -5,7 +5,6 @@
 
     try{
         $db = new PDO($dsn,$username,$password);
-        //echo "เชื่อมต่อสมบูรณ์";
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e){
         echo $e->getMessage();

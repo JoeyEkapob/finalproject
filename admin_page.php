bb14cbcc1ecc1dd5ed9b41e68d06a69f73d4170a
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-01-08 00:33:36 +0700

    ยังไม่เรียบร้อยดี

--- admin_page.php
+++ admin_page.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    require_once 'connect.php';
+    if(!isset($_SESSION['admin_login'])){
+         $_SESSION['error'] = '<center>กรุณาล็อกอิน</center>'; 
+        header('location:login.php');
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php include 'menubar.php'?>
+<body>
+        <?php 
+            if (isset($_SESSION['admin_login'])) {
+                $admin_id = $_SESSION['admin_login'];
+            // echo $admin_id ;
+                $stmt = $db->query("SELECT * FROM user WHERE user_id = $admin_id");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        ?>
+       <h3 class="mt-4">Welcome Admin, <?php echo $row['firstname'] . ' ' . $row['lastname'] ?></h3> 
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+    </div>
 </body>
 </html>
--- user_page.php
+++ user_page.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    require_once 'connect.php';
+    if(!isset($_SESSION['user_login'])){
+        $_SESSION['error'] = '<center>กรุณาล็อกอิน</center>';
+        header('location:login.php');
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +15,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+    <?php include 'menubar.php'?>
 <body>
-    
+        <?php 
+            if (isset($_SESSION['user_login'])) {
+                $user_id = $_SESSION['user_login'];
+            // echo $admin_id ;
+                $stmt = $db->query("SELECT * FROM user WHERE user_id = $user_id");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        //print_r($_SESSION['user_login']);
+         ?>
+        <h3 class="mt-4">Welcome, <?php echo $row['status'].' '.$row['firstname'] . ' ' . $row['lastname'] ?></h3>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
 </body>
 </html>

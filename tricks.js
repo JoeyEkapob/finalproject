04a8f4d63a50965feae003b57a84a66414cb5ca7
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-01-08 00:32:44 +0700

    ไม่ได้เเก้อะไรเพิ่มเติม

--- tricks.js
+++ tricks.js
@@ -1,11 +1,11 @@
-function showpass(){
-    var pass = document.getElementById('password');
-    if(pass.type == 'password'){
-        pass.type = 'text';
+function showpass(){ //เเสดง Password
+  var pass = document.getElementById('password');
+  if(pass.type == 'password'){
+      pass.type = 'text';
 
-    }else if(pass.type == 'text'){
+  }else if(pass.type == 'text'){
 
-        pass.type = 'password';
-    }
+      pass.type = 'password';
+  }
 
 }

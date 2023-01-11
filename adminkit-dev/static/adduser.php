ad767c3fa84967a541c818f3db52df2e146ae8c9
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-01-11 16:42:45 +0700

    เเก้

--- adminkit-dev/static/adduser.php
+++ adminkit-dev/static/adduser.php
@@ -1,8 +1,10 @@
 <?php 
 
     session_start();
+    
     require_once 'connect.php';
 
+    $targetDir = "adminkit-dev/static/img/avatars/";
     if (isset($_POST['signup'])) {
         $firstname = $_POST['firstname'];
         $lastname = $_POST['lastname'];
@@ -10,41 +12,45 @@
         $password = $_POST['password'];
         $c_password = $_POST['c_password'];
         $status = $_POST['type'];
-
+        $fileName = basename($_FILES["file"]["name"]);
+        $targetFilePath = $targetDir . $fileName;
+        $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+        print_r ($fileName);
+       exit;
         if (empty($firstname)) {
             $_SESSION['error'] = 'กรุณากรอกชื่อ';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
          } else if (empty($lastname)) {
             $_SESSION['error'] = 'กรุณากรอกนามสกุล';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if (empty($email)) {
             $_SESSION['error'] = 'กรุณากรอกอีเมล';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $_SESSION['error'] = 'รูปแบบอีเมลไม่ถูกต้อง';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if (empty($password)) {
             $_SESSION['error'] = 'กรุณากรอกรหัสผ่าน';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
             $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if (empty($c_password)) {
             $_SESSION['error'] = 'กรุณายืนยันรหัสผ่าน';
-            header("location: adduser_page.php");
+            header("location: pages-sign-up.php");
         } else if ($password != $c_password) {
             $_SESSION['error'] = 'รหัสผ่านไม่ตรงกัน';
-            header("location: adduser_page.php"); 
+            header("location: pages-sign-up.php"); 
         } else {
             try {
                 $check_email = $db->prepare("SELECT email FROM user WHERE email = :email");
                 $check_email->bindParam(":email", $email);
                 $check_email->execute();
                 $row = $check_email->fetch(PDO::FETCH_ASSOC);
-                //print_r ($status);
+               // print_r ($row);
                 if ($row['email'] == $email) {
-                    $_SESSION['warning'] = "มีอีเมลนี้อยู่ในระบบแล้ว <a href='login.php'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("location: adduser_page.php");
+                    $_SESSION['warning'] = "มีอีเมลนี้อยู่ในระบบแล้ว <a href='sign-in.php'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: pages-sign-up.php");
                 } else if (!isset($_SESSION['error'])) {
                     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
                     $stmt = $db->prepare("INSERT INTO user(firstname, lastname, email, password, status) 
@@ -56,10 +62,10 @@
                     $stmt->bindParam(":status", $status);
                     $stmt->execute();
                     $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='login.php' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
-                    header("location: adduser_page.php");
+                    header("location: pages-sign-up.php");
                 } else {
                     $_SESSION['error'] = "มีบางอย่างผิดพลาด";
-                    header("location: adduser_page.php");
+                    header("location: pages-sign-up.php");
                 } 
  
             } catch(PDOException $e) {
--- adminkit-dev/static/pages-sign-up.php
+++ adminkit-dev/static/pages-sign-up.php
@@ -16,81 +16,100 @@
 <body>
 <?php include "sidebar.php"?>
 <form action="adduser.php" method="post">
-	<main class="d-flex w-100">
-		<div class="container d-flex flex-column">
-					<div class="text-center mt-4">
-							<h1 class="h2">Add user</h1>
-							
-						</div>
-
-						<div class="card">
+<?php if(isset($_SESSION['error'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php 
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                </div>
+            <?php } ?>
+            <?php if(isset($_SESSION['success'])) { ?>
+                <div class="alert alert-success" role="alert">
+                    <?php 
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                    ?>
+                </div>
+            <?php } ?>
+            <?php if(isset($_SESSION['warning'])) { ?>
+                <div class="alert alert-warning" role="alert">
+                    <?php 
+                        echo $_SESSION['warning'];
+                        unset($_SESSION['warning']);
+                    ?>
+                </div>
+            <?php } ?>
+<main class="content">
+				<div class="container-fluid p-0">
+					<h1 class="h3 mb-3">Add user</h1>
+				</div>
+					<div class="row">
+						<div class="card">		
 							<div class="card-body">
-								<div class="m-sm-4">
-									<form>
-										<div class="row">
-											<div class="col-md-6 border-right">
-												<div class="form-group">
-													<label for="" class="control-label">First Name</label>
-													<input type="text" name="firstname" class="form-control form-control-sm" required value="">
-												</div>
-												<div class="form-group">
-													<label for="" class="control-label">Last Name</label>
-													<input type="text" name="lastname" class="form-control form-control-sm" required value="">
-												</div>
-												
-												<div class="mb-12">
-													<label for="" class="control-label">User Role</label>
-														<select name="type" id="type" class="form-control">
-															<option value="5" >เจ้าหน้าที่</option>
-															<option value="4" >หัวสาขา</option>
-															<option value="4" >หัวหน้าหน่วย</option>
-															<option value="4" >ผู้ชวยรองรองคณบดีฝ่ร</option>
-															<option value="2" >คณบดี</option>ายวิชาการ</option>
-															<option value="3" >รองคณบดีฝ่ายวิชากา
-															<option value="1" >Admin</option>
-														</select>
-												</div> 							
-	
-												<div class="mb-3">	
-													<div class="form-group">
-														<label for="" class="control-label">Avatar</label>	
-														<input class="form-control border border-primary" type="file" id="formFile" accept="image/jpeg, image/png, image/jpg"> 
-													</div>
-												</div>
+								<div class="row">
+									<div class="col-md-6">
+										<div class="mb-3">
+											<label for="" class="control-label">First Name</label>
+											<input type="text" name="firstname" class="form-control form-control" required value="">
+										</div>
+										<div class="mb-3">
+											<label for="" class="control-label">Last Name</label>
+											<input type="text" name="lastname" class="form-control form-control" required value="">
+										</div>
+										
+										<div class="mb-3">
+											<label for="" class="control-label">User Role</label>
+												<select name="type" id="type" class="form-control">
+													<option value="5" >เจ้าหน้าที่</option>
+													<option value="4" >หัวสาขา</option>
+													<option value="4" >หัวหน้าหน่วย</option>
+													<option value="4" >ผู้ชวยรองรองคณบดีฝ่ร</option>
+													<option value="2" >คณบดี</option>ายวิชาการ</option>
+													<option value="3" >รองคณบดีฝ่ายวิชากา
+													<option value="1" >Admin</option>
+												</select>
+										</div> 								
+									</div>	
+									<div class="col-md-6">
+											<div class="mb-3">
+												<label class="control-label">Email</label>
+												<input type="email" class="form-control form-control" name="email" required value="">
+												<small id="#msg"></small>
 											</div>
-											
-											<div class="col-md-6">
-												<div class="form-group">
-													<label class="control-label">Email</label>
-													<input type="email" class="form-control form-control-sm" name="email" required value="">
-													<small id="#msg"></small>
-												</div>
-												<div class="form-group">
-													<label class="control-label">Password</label>
-													<input type="password" class="form-control form-control-sm" name="password">
-													
-												</div>
-												<div class="form-group">
-													<label class="label control-label">Confirm Password</label>
-													<input type="password" class="form-control form-control-sm" name="cpass">
-													<small id="pass_match" data-status=''></small>
-												</div>
+											<div class="mb-3">
+												<label class="control-label">Password</label>
+												<input type="password" class="form-control form-control" name="password">	
 											</div>
+											<div class="mb-3">
+												<label class="label control-label">Confirm Password</label>
+												<input type="password" class="form-control form-control" name="c_password">	
+											</div>
+									</div>
+										<div class="mb-3">
+											<div class="form-group">
+												<label for="" class="control-label">Avatar</label>	
+												<input  type="file"  name="file" class="form-control border border-primary"  accept="image/jpeg, image/png, image/jpg">
+												<p class="small mb-0 mt-2"><b>Note:</b> Only JPG, JPEG, PNG & GIF files are allowed to upload</p> 
+											</div>
+										</div>
+										<div class="mb-4">
+										
 										</div>
 										<hr>
 										<div class="col-lg-12 text-right justify-content-center d-flex">
-											<button class="btn btn-primary">Save</button>
+
+											<button class="btn btn-primary" name="signup">Save</button>
+
 											<button class="btn btn-secondary" type="button" >Cancel</button>
 										</div>
-									</form>
-								</div>
+									</div>
+		
 							</div>
 						</div>
-
-			</div>
-		</div>
-	</main>
-</from>
+					</div>			
+			</main>
+</from> 
 
 	<script src="js/app.js"></script>
 

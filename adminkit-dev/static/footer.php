93a35f4c1f77a48353617e8706c5c6da5ee5bd0c
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-02-13 14:06:24 +0700

    เปิด js

--- adminkit-dev/static/footer.php
+++ adminkit-dev/static/footer.php
@@ -2,7 +2,7 @@
  <script src="js/app.js"></script> 
 
 <!-- // js bootstrap -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> 
 
 
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>

9ffd8ca4de341fcab205fe736aa21469be52b75d
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-02-21 12:39:25 +0700

    ลบ

--- adminkit-dev/static/database/finalproject.sql
+++ adminkit-dev/static/database/finalproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2023 at 07:02 PM
+-- Generation Time: Feb 21, 2023 at 06:37 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,59 @@ SET time_zone = "+00:00";
 --
 -- Database: `finalproject`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file`
+--
+
+CREATE TABLE `file` (
+  `file_id` int(30) NOT NULL,
+  `create_date` timestamp NULL DEFAULT current_timestamp(),
+  `filelist` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `file`
+--
+
+INSERT INTO `file` (`file_id`, `create_date`, `filelist`) VALUES
+(55, '2023-02-20 13:53:46', NULL),
+(56, '2023-02-20 14:08:01', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_item_project`
+--
+
+CREATE TABLE `file_item_project` (
+  `file_item_project` int(30) NOT NULL,
+  `file_id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `filename` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `file_item_project`
+--
+
+INSERT INTO `file_item_project` (`file_item_project`, `file_id`, `project_id`, `filename`) VALUES
+(90, 55, 163, 'Lab06.pdf'),
+(91, 55, 163, 'EC08.pdf'),
+(92, 56, 164, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `file_item_task`
+--
+
+CREATE TABLE `file_item_task` (
+  `file_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -45,7 +98,12 @@ INSERT INTO `job_type` (`id_jobtype`, `name_jobtype`, `status`) VALUES
 (5, 'งานประจำ', 1),
 (6, 'งานวิจัย', 1),
 (7, 'การบริการวิชาการ', 1),
-(8, 'รับสมัครคนศประจำปี', 1);
+(8, 'รับสมัครคนศประจำปี', 1),
+(9, 'test', 2),
+(10, 'test', 2),
+(11, 'test1', 2),
+(12, 'test 2', 2),
+(13, 'test', 2);
 
 -- --------------------------------------------------------
 
@@ -84,9 +142,9 @@ CREATE TABLE `project` (
   `description` text NOT NULL,
   `status_1` tinyint(2) NOT NULL,
   `create_project` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `start_date` date NOT NULL DEFAULT current_timestamp(),
-  `end_date` date NOT NULL DEFAULT current_timestamp(),
-  `file_project` varchar(255) DEFAULT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `file_id` int(30) DEFAULT NULL,
   `manager_id` int(30) NOT NULL,
   `status_2` int(30) NOT NULL,
   `id_jobtype` int(30) NOT NULL
@@ -96,8 +154,9 @@ CREATE TABLE `project` (
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`project_id`, `name_project`, `description`, `status_1`, `create_project`, `start_date`, `end_date`, `file_project`, `manager_id`, `status_2`, `id_jobtype`) VALUES
-(36, 'test 5 test', '                                                                                                                                                                                                                             test 5  test\r\n                                              \r\n                                              \r\n                                              \r\n                                              \r\n                                              \r\n                                            ', 1, '2023-02-08 17:53:20', '2023-02-09', '2023-02-10', NULL, 1, 1, 3);
+INSERT INTO `project` (`project_id`, `name_project`, `description`, `status_1`, `create_project`, `start_date`, `end_date`, `file_id`, `manager_id`, `status_2`, `id_jobtype`) VALUES
+(163, 'test', 'test', 1, '2023-02-20 13:53:46', '2023-02-20', '2023-02-25', 55, 1, 3, 3),
+(164, 'test 2 ', 'tset', 1, '2023-02-20 14:08:01', '2023-02-20', '2023-02-23', 56, 4, 2, 8);
 
 -- --------------------------------------------------------
 
@@ -115,9 +174,13 @@ CREATE TABLE `project_list` (
 --
 
 INSERT INTO `project_list` (`project_id`, `user_id`) VALUES
-(36, 2),
-(36, 4),
-(36, 6);
+(163, 2),
+(163, 3),
+(163, 4),
+(163, 6),
+(163, 7),
+(164, 5),
+(164, 7);
 
 -- --------------------------------------------------------
 
@@ -130,8 +193,8 @@ CREATE TABLE `task_list` (
   `name_tasklist` varchar(255) NOT NULL,
   `description_task` varchar(200) NOT NULL,
   `status_task` tinyint(2) NOT NULL,
-  `strat_date_task` date NOT NULL,
-  `end_date_task` date NOT NULL,
+  `strat_date_task` datetime NOT NULL,
+  `end_date_task` datetime NOT NULL,
   `file_task` varchar(45) DEFAULT NULL,
   `project_id` int(30) NOT NULL,
   `user_id` int(30) NOT NULL
@@ -142,7 +205,9 @@ CREATE TABLE `task_list` (
 --
 
 INSERT INTO `task_list` (`task_id`, `name_tasklist`, `description_task`, `status_task`, `strat_date_task`, `end_date_task`, `file_task`, `project_id`, `user_id`) VALUES
-(15, 'test', 'test', 1, '2023-02-09', '2023-02-10', NULL, 36, 6);
+(10, 'test', '  test  \r\n                        ', 1, '2023-02-20 20:53:00', '2023-02-28 21:00:00', NULL, 163, 4),
+(11, 'test 2', 'test 2', 1, '2023-02-20 20:57:00', '2023-02-25 20:00:00', NULL, 163, 7),
+(12, 'test 10', 'fsdfsf', 1, '2023-02-20 21:08:00', '2023-02-23 21:09:00', NULL, 164, 7);
 
 -- --------------------------------------------------------
 
@@ -179,6 +244,28 @@ INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `email`, `password`, `ro
 --
 
 --
+-- Indexes for table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`file_id`);
+
+--
+-- Indexes for table `file_item_project`
+--
+ALTER TABLE `file_item_project`
+  ADD PRIMARY KEY (`file_item_project`),
+  ADD KEY `file_id_idx` (`file_id`),
+  ADD KEY `project_id_pj_idx` (`project_id`);
+
+--
+-- Indexes for table `file_item_task`
+--
+ALTER TABLE `file_item_task`
+  ADD PRIMARY KEY (`task_id`,`file_id`),
+  ADD KEY `file_id_tk_idx` (`file_id`),
+  ADD KEY `task_id_fi_idx` (`task_id`);
+
+--
 -- Indexes for table `job_type`
 --
 ALTER TABLE `job_type`
@@ -203,7 +290,7 @@ ALTER TABLE `project`
 ALTER TABLE `project_list`
   ADD PRIMARY KEY (`project_id`,`user_id`),
   ADD KEY `project_id_fk_idx` (`project_id`),
-  ADD KEY `user_id_idx` (`user_id`);
+  ADD KEY `user_id_fk1` (`user_id`);
 
 --
 -- Indexes for table `task_list`
@@ -225,26 +312,58 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `file`
+--
+ALTER TABLE `file`
+  MODIFY `file_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `file_item_project`
+--
+ALTER TABLE `file_item_project`
+  MODIFY `file_item_project` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+
+--
 -- AUTO_INCREMENT for table `job_type`
 --
 ALTER TABLE `job_type`
-  MODIFY `id_jobtype` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_jobtype` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `project_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `project_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
 
 --
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `task_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `task_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `file_item_project`
+--
+ALTER TABLE `file_item_project`
+  ADD CONSTRAINT `file_id_pj` FOREIGN KEY (`file_id`) REFERENCES `file` (`file_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `project_id_pj` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `file_item_task`
+--
+ALTER TABLE `file_item_task`
+  ADD CONSTRAINT `file_id_tk` FOREIGN KEY (`file_id`) REFERENCES `file` (`file_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `task_id_fi` FOREIGN KEY (`task_id`) REFERENCES `task_list` (`task_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `project`
@@ -256,15 +375,15 @@ ALTER TABLE `project`
 -- Constraints for table `project_list`
 --
 ALTER TABLE `project_list`
-  ADD CONSTRAINT `project_id_fk1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `user_id_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `project_id_fk1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `task_list`
 --
 ALTER TABLE `task_list`
-  ADD CONSTRAINT `project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`),
-  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `project_id` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user`

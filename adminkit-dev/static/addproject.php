e010afa6e1df6ed1c67cbd622b1552409cdae4a0
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-02-23 00:45:50 +0700

    เเก้ไข addยproject

--- adminkit-dev/static/addproject.php
+++ adminkit-dev/static/addproject.php
@@ -4,35 +4,35 @@
 if(isset($_POST['addpro'])){
     //print_r($_POST['users_id']); 
      
-      $status1 = 1;
-      $proname = $_POST['proname'];
-      $start_date = $_POST['start_date'];
-      $end_date = $_POST['end_date'];
-      $description =$_POST['description'];
-      $manager_id=$_SESSION['user_login'];
-      $status2=$_POST['status2'];
-      $file_project = null;
-      $job = $_POST['job'];
+   $status1 = 1;
+   $proname = $_POST['proname'];
+   $start_date = $_POST['start_date'];
+   $end_date = $_POST['end_date'];
+   $description =$_POST['description'];
+   $manager_id=$_SESSION['user_login'];
+   $status2=$_POST['status2'];
+   $file_project = null;
+   $job = $_POST['job'];
 
-      $users_id1=$_POST['users_id']; // รับค้ามาเป็น arery id เดียว
-      //$numbers_string = implode(",", $users_id1); // implode ลูกน้ำเข้าไปเเล้วทำให้เป็น string
-      $users_id = explode(",", $users_id1); // เเล้วก็นำ string มาทำเป็น array หลาย id 
-      $filelist=null;
+   $users_id1=$_POST['users_id']; // รับค้ามาเป็น arery id เดียว
+   //$numbers_string = implode(",", $users_id1); // implode ลูกน้ำเข้าไปเเล้วทำให้เป็น string
+   $users_id = explode(",", $users_id1); // เเล้วก็นำ string มาทำเป็น array หลาย id 
+   $filelist=null;
 
     
 
 
-     if (empty($proname)) {
-       $_SESSION['error'] = 'กรุณากรอกชื่อโปรเจค';
-       header('location:addproject_page.php');
+   if (empty($proname)) {
+      $_SESSION['error'] = 'กรุณากรอกชื่อโปรเจค';
+      header('location:addproject_page.php');
 
    } else if (empty($start_date)) {
-        $_SESSION['error'] ='กรุณากรอกวันที่เริ่ม';
-        header('location:addproject_page.php');
+      $_SESSION['error'] ='กรุณากรอกวันที่เริ่ม';
+      header('location:addproject_page.php');
 
    } else if (empty($end_date)) {
-    $_SESSION['error'] = 'กรุณากรอกวันที่สิ้นสุด';
-    header('location:addproject_page.php');
+      $_SESSION['error'] = 'กรุณากรอกวันที่สิ้นสุด';
+      header('location:addproject_page.php');
 
    } else if (empty($users_id1)) {
       $_SESSION['error'] = 'ใส่ชื่อสมาชิก';
@@ -40,7 +40,7 @@ if(isset($_POST['addpro'])){
    
    } else if(!isset($_SESSION['error'])) {
        $inserstmtpro = $db->prepare("INSERT INTO project(name_project, description, status_1,start_date, end_date,file_id, manager_id,status_2,id_jobtype) 
-      VALUES(:proname,:description,:status,:start_date,:end_date,:file_id,:manager_id,:status_2,:id_job)");
+                                          VALUES(:proname,:description,:status,:start_date,:end_date,:file_id,:manager_id,:status_2,:id_job)");
        $inserstmtpro->bindParam(":proname", $proname);
        $inserstmtpro->bindParam(":description", $description);
        $inserstmtpro->bindParam(":status", $status1);
@@ -54,55 +54,55 @@ if(isset($_POST['addpro'])){
        $lastId = $db->lastInsertId();
     
       foreach ($users_id as $id => $users_id){
-      $inserstmtprolist= $db->prepare("INSERT INTO project_list(project_id,user_id) VALUES(:project_id,:user_id)");
-      $inserstmtprolist->bindParam(":project_id", $lastId );
-      $inserstmtprolist->bindParam(":user_id", $users_id );
-      $inserstmtprolist->execute(); 
-         } 
+         $inserstmtprolist= $db->prepare("INSERT INTO project_list(project_id,user_id) VALUES(:project_id,:user_id)");
+         $inserstmtprolist->bindParam(":project_id", $lastId );
+         $inserstmtprolist->bindParam(":user_id", $users_id );
+         $inserstmtprolist->execute(); 
+      } 
 
-      }else {
+   }else {
       $_SESSION['error']= "มีบางอย่างผิดพลาด";
       header('location:addproject_page.php');
   
-      }       
+   }       
  
-         if(!isset($_SESSION['error'])) {
-            $inserfile = $db->prepare("INSERT INTO file(filelist) VALUES(:filelist)");
-            $inserfile->bindParam(":filelist",$filelist);
-            $inserfile->execute(); 
-            $lastIdfile = $db->lastInsertId(); 
+   if(!isset($_SESSION['error'])) {
+      $inserfile = $db->prepare("INSERT INTO file(filelist) VALUES(:filelist)");
+      $inserfile->bindParam(":filelist",$filelist);
+      $inserfile->execute(); 
+      $lastIdfile = $db->lastInsertId(); 
 
-            $files = $_FILES['files'];
-            foreach ($files['name'] as $i => $file_name) {
-            $file_tmp = $files['tmp_name'][$i];
-            $file_dest = $file_name; 
-            move_uploaded_file($file_tmp,"img/file/".$file_dest);
-            
-            $inserfile_item_porject = $db->prepare("INSERT INTO file_item_project(file_id,project_id,filename) 
-            VALUES(:file_id,:project_id,:filename)");
-            $inserfile_item_porject->bindParam(":file_id",$lastIdfile );
-            $inserfile_item_porject->bindParam(":project_id",$lastId);
-            $inserfile_item_porject->bindParam(":filename",$file_dest);
-            $inserfile_item_porject->execute(); 
-            
-            
-           
-            }
-
-         $uploadstmtproject = $db->prepare('UPDATE project SET file_id =:file_id WHERE project_id = :project_id');
-         $uploadstmtproject->bindParam(':file_id',$lastIdfile);
-         $uploadstmtproject->bindParam(':project_id',$lastId);
-         $uploadstmtproject->execute(); 
-
-         $_SESSION['success'] = "เพิ่มโปรเจคเรียบร้อยแล้ว! ";
-         header('location:project_list.php');
-            }else {
-               $_SESSION['error']= "มีบางอย่างผิดพลาด";
-               header('location:addproject_page.php');
+      $files = $_FILES['files'];
+      foreach ($files['name'] as $i => $file_name) {
+         $file_tmp = $files['tmp_name'][$i];
+         $file_dest = $file_name; 
+         $file_data = "img/file/";
+         move_uploaded_file($file_tmp,$file_data.$file_dest);
          
-            }       
+         $inserfile_item_porject = $db->prepare("INSERT INTO file_item_project(file_id,project_id,filename) 
+         VALUES(:file_id,:project_id,:filename)");
+         $inserfile_item_porject->bindParam(":file_id",$lastIdfile );
+         $inserfile_item_porject->bindParam(":project_id",$lastId);
+         $inserfile_item_porject->bindParam(":filename",$file_dest);
+         $inserfile_item_porject->execute(); 
+         
+               
+            
+      }
 
-         }
+      $uploadstmtproject = $db->prepare('UPDATE project SET file_id =:file_id WHERE project_id = :project_id');
+      $uploadstmtproject->bindParam(':file_id',$lastIdfile);
+      $uploadstmtproject->bindParam(':project_id',$lastId);
+      $uploadstmtproject->execute(); 
+
+      $_SESSION['success'] = "เพิ่มโปรเจคเรียบร้อยแล้ว! ";
+      header('location:project_list.php');
+   }else {
+      $_SESSION['error']= "มีบางอย่างผิดพลาด";
+      header('location:addproject_page.php');
+   }       
+
+}
 
 /* $stmt = $db->query("SELECT * FROM project where project_id=2");
 $stmt->execute();

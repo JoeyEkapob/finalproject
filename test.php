6bf88144ba3fdc7cd364a5149ebcc0ea85921cda
JoeyEkapob <107201773+JoeyEkapob@users.noreply.github.com>
2023-05-03 20:04:21 +0700

    เพิ่ม

--- test.php
+++ test.php
@@ -1,7 +1,16 @@
 <?php
+/*  1. เรียกไฟล์ "fpdf.php" (เครื่องมือช่วยในการออกแบบรายงานเป็น pdf)
+    Link : http://fpdf.org/
+*/
+require("fpdf/fpdf.php");
+$pdf = new FPDF(); //สร้าง Obj.
+$pdf->AddFont('THSarabun','','THSarabun.php');
+$pdf->AddFont('THSarabun','','THSarabun Bold.php');
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+$pdf->AddPage();
+$pdf->SetFont('THSarabun','',16);
+$pdf->Cell(40,10,iconv('UTF-8','cp874','Hello World! สารสนเทศ'));
+$pdf->SetFont('THSarabun','',27);
+$pdf->Cell(40,10,iconv('UTF-8','cp874','Hello World! สารสนเทศ'));
+$pdf->Output();
+?>
